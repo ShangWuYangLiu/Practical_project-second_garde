74f87b6682b400ccb3d280710dd987f4400190ac
ShangWuYangLiu <105548921+ShangWuYangLiu@users.noreply.github.com>
2022-07-21 21:48:52 +0800

    Update sm3.h

--- SM3/sm3.h
+++ SM3/sm3.h
@@ -1,10 +1,17 @@
-#ifndef _SM3_H_
-#define _SM3_H_
+#pragma once
 
-extern unsigned int sm3(
-	unsigned char* ucpSrcData,
-	unsigned int nSrcLen,
-	unsigned char* ucpHash,
-	unsigned int* nHashLen);
+#define SM3_HASH_SIZE 32
+namespace SM3 {
+	/*哈希值向量大小，单位为字节*/
+	typedef struct SM3Context {
+		unsigned int intermediateHash[SM3_HASH_SIZE / 4];
+		unsigned char messageBlock[64];//512位的数据块，是迭代压缩的对象
+	} SM3Context;
 
-#endif#pragma once
+	unsigned char* SM3_optimize(unsigned char* message,
+		unsigned int messageLen, unsigned char digest[SM3_HASH_SIZE]);
+	unsigned char* SM3(unsigned char* message,
+		unsigned int messageLen, unsigned char digest[SM3_HASH_SIZE]);
+	std::vector<uint32_t> sm3_hash();
+	std::vector<uint32_t> sm3_hash_optimize();
+}
