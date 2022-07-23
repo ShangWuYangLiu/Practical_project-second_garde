4810d7ef6ccd737e0cd36835b72ca55f1de29f9a
ShangWuYangLiu <105548921+ShangWuYangLiu@users.noreply.github.com>
2022-07-23 20:27:19 +0800

    Update sm2.py

--- SM2/sm2.py
+++ SM2/sm2.py
@@ -1,41 +1,192 @@
 import secrets
 from hashlib import sha256
-import utils
-
 from gmssl import sm3, func
 
+#y^2=x^3+7
 A = 0
 B = 7
-
+#有限域的阶
+P = 115792089237316195423570985008687907853269984665640564039457584007908834671663
+#椭圆曲线的阶
+N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
 G_X = 55066263022277343669578718895168534326250603453777594175500187360389116729240
 G_Y = 32670510020758816978083085130507043184471273380659243275938904335757337482424
-
 G = (G_X, G_Y)
-P = 115792089237316195423570985008687907853269984665640564039457584007908834671663
-N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
 
 
-def precompute(ID, a, b, G_X, G_Y, x_A, y_A):
+
+#勒让德符号
+def Legendre(y,p):
+    return pow(y,(p - 1) // 2,p)
+
+#Tonelli-Shanks算法，求二次剩余(x^2=y mod p)
+def Tonelli_Shanks(y,p):
+    assert Legendre(y,p) == 1
+    if p % 4 == 3:
+        return pow(y,(p + 1) // 4,p)
+    q = p - 1
+    s = 0
+    while q % 2 == 0:
+        q = q // 2
+        s += 1
+    for z in range(2,p):
+        if Legendre(z,p) == p - 1:
+            c = pow(z,q,p)
+            break
+    r = pow(y,(q + 1) // 2,p)
+    t = pow(y,q,p)
+    m = s
+    if t % p == 1:
+        return r
+    else:
+        i = 0
+        while t % p != 1:
+            temp = pow(t,2**(i+1),p)
+            i += 1
+            if temp % p == 1:
+                b = pow(c,2**(m - i - 1),p)
+                r = r * b % p
+                c = b * b % p
+                t = t * c % p
+                m = i
+                i = 0
+        return r
+
+#扩展欧几里得算法:返回最大公因子和系数
+def extended_euclidean_algorithm(j, k):
+    if j == k:
+        return (j, 1, 0)
+    else:
+        i = 0
+        j_array = [j]
+        k_array = [k]
+        q_array = []
+        r_array = []
+
+        prev_r_is_zero = False
+
+        while not (prev_r_is_zero):
+            q_array.append(k_array[i]//j_array[i])
+            r_array.append(k_array[i]%j_array[i])
+            k_array.append(j_array[i])
+            j_array.append(r_array[i])
+            i += 1
+            if r_array[i-1] == 0:
+                prev_r_is_zero = True
+        i -= 1
+        gcd = j_array[i]
+        x_array = [1]
+        y_array = [0]
+
+        i -= 1
+        total_steps = i
+
+        while i >= 0:
+            y_array.append(x_array[total_steps-i])
+            x_array.append(y_array[total_steps-i] - q_array[i]*x_array[total_steps-i])
+            i -= 1
+
+        return (gcd, x_array[-1], y_array[-1])
+
+#模逆运算
+def mod_inverse(j, n):
+    (gcd, x, y) = extended_euclidean_algorithm(j, n)
+
+    if gcd == 1:
+        return x%n
+    else:
+        return -1
+
+#椭圆曲线加法运算
+def elliptic_add(p, q):
+    if p == 0 and q == 0: return 0
+    elif p == 0: return q
+    elif q == 0: return p
+    else:
+        # Swap p and q if px > qx.
+        if p[0] > q[0]:
+            temp = p
+            p = q
+            q = temp
+        r = []
+        slope = (q[1] - p[1])*mod_inverse(q[0] - p[0], P) % P
+
+        r.append((slope**2 - p[0] - q[0]) % P)
+        r.append((slope*(p[0] - r[0]) - p[1]) % P)
+
+        return (r[0], r[1])
+
+#2P
+def elliptic_double(p):
+    r = []
+
+    slope = (3*p[0]**2 + A)*mod_inverse(2*p[1], P) % P
+
+    r.append((slope**2 - 2*p[0])%P)
+    r.append((slope*(p[0] - r[0]) - p[1])%P)
+
+    return (r[0], r[1])
+
+#椭圆曲线乘法运算
+def elliptic_multiply(s, p):
+    n = p
+    r = 0 # 0 representing a point at infinity
+
+    s_binary = bin(s)[2:] # convert s to binary and remove the "0b" in the beginning
+    s_length = len(s_binary)
+
+    for i in reversed(range(s_length)):
+        if s_binary[i] == '1':
+            r = elliptic_add(r, n)
+        n = elliptic_double(n)
+
+    return r
+
+#得到十进制数的二进制长度
+def get_bit_num(x):
+    if isinstance(x, int):
+        num = 0
+        tmp = x >> 64
+        while tmp:
+            num += 64
+            tmp >>= 64
+        tmp = x >> num >> 8
+        while tmp:
+            num += 8
+            tmp >>= 8
+        x >>= num
+        while x:
+            num += 1
+            x >>= 1
+        return num
+    elif isinstance(x, str):
+        return len(x.encode()) << 3
+    elif isinstance(x, bytes):
+        return len(x) << 3
+    return 0
+
+
+def precompute(ID, a, b, G_X, G_Y, x_A, y_A):#ID，椭圆曲线参数a、b,G点x、y,公钥x、y
     a = str(a)
     b = str(b)
     G_X = str(G_X)
     G_Y = str(G_Y)
     x_A = str(x_A)
     y_A = str(y_A)
-    ENTL = str(utils.get_bit_num(ID))
+    ENTL = str(get_bit_num(ID))
 
     joint = ENTL + ID + a + b + G_X + G_Y + x_A + y_A
     joint_b = bytes(joint, encoding='utf-8')
     digest = sm3.sm3_hash(func.bytes_to_list(joint_b))
     return int(digest, 16)
 
-
+#生成公私钥对
 def generate_key():
     private_key = int(secrets.token_hex(32), 16)
-    public_key = utils.elliptic_multiply(private_key, G)
+    public_key = elliptic_multiply(private_key, G)
     return private_key, public_key
 
-
+#签名函数
 def sign(private_key, message, Z_A):
     _M = Z_A + message
     _M_b = bytes(_M, encoding='utf-8')
@@ -43,14 +194,14 @@ def sign(private_key, message, Z_A):
     e = int(e, 16)
 
     k = secrets.randbelow(P)
-    random_point = utils.elliptic_multiply(k, G)
+    random_point = elliptic_multiply(k, G)
 
-    print(random_point, 'randpoint')
+    #print(random_point, 'randpoint')
     r = (e + random_point[0]) % N
-    s = (utils.mod_inverse(1 + private_key, N) * (k - r * private_key)) % N
+    s = (mod_inverse(1 + private_key, N) * (k - r * private_key)) % N
     return (r, s)
 
-
+#验证函数
 def verify(public_key, ID, message, signature):
     r = signature[0]
     s = signature[1]
@@ -63,26 +214,29 @@ def verify(public_key, ID, message, signature):
     e = int(e, 16)
     t = (r + s) % N
 
-    point = utils.elliptic_multiply(s, G)
-    point1 = utils.elliptic_multiply(t, public_key)
-    point = utils.elliptic_add(point, point1)
+    point = elliptic_multiply(s, G)
+    point1 = elliptic_multiply(t, public_key)
+    point = elliptic_add(point, point1)
 
     x1 = point[0]
     x2 = point[1]
     R = (e + x1) % N
-    print('r', r)
-    print(R)
 
     return R == r
 
 
 if __name__ == '__main__':
     prikey, pubkey = generate_key()
-    print('pk：', pubkey)
-    message = "hello world!"
-    ID = '202000460012'
+    message = "202000460012"
+    print('='*75)
+    print("消息为:",message)
+    print('='*75)
+    print('公钥为：', pubkey)
+    ID = '1234567812345678'#sm2使用固定值
     Z_A = precompute(ID, A, B, G_X, G_Y, pubkey[0], pubkey[1])
     signature = sign(prikey, message, str(Z_A))
-    print("sign: ", signature)
+    print('=' * 75)
+    print("签名为: ", signature)
     if verify(pubkey, ID, message, signature) == 1:
-        print('ok')
+        print('=' * 75)
+        print('验证结果:True')
