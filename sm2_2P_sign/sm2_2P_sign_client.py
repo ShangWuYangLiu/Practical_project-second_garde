35fb7c3baeb21d3d2375ed7ba571b4bd29681789
ShangWuYangLiu <105548921+ShangWuYangLiu@users.noreply.github.com>
2022-07-29 22:53:23 +0800

    Update sm2_2P_sign_client.py

--- sm2_2P_sign/sm2_2P_sign_client.py
+++ sm2_2P_sign/sm2_2P_sign_client.py
@@ -1,72 +1,46 @@
-import pre_SM2
+import socket
+from new_sm2 import *
 import secrets
 from gmssl import sm3, func
-import socket
-import time
 
-def step1():
-    """step 1 of left
-    :return d1, P1
-    """
-    d1 = secrets.randbelow(pre_SM2.N)
-    tmp = pre_SM2.inv(d1, pre_SM2.N)
-    P1 = pre_SM2.EC_multi(tmp, pre_SM2.G)
-    return d1, P1
+def beginAC():
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def step2(Z, M):
-    """step 2 of left
-    :param Z: identifier for both parties
-    :param M: massage
-    :return k1, Q1, e
-    """
-    m = Z + M
-    m_b = bytes(m, encoding='utf-8')
-    e = sm3.sm3_hash(func.bytes_to_list(m_b))
-    k1 = secrets.randbelow(pre_SM2.N)
-    Q1 = pre_SM2.EC_multi(k1, pre_SM2.G)
-    return k1, Q1, e
+    # 1:生成私钥d1和P1,向服务器发送P1
+    d1 = secrets.randbelow(N)
+    P1 = elliptic_mult(mod_inverse(d1, N),G)
+    sdata = str(P1[0]) + ',' + str(P1[1])
+    client.sendto(sdata.encode(), ("127.0.0.1", 12345))
 
-def step3(d1, k1, r, s2, s3):
-    """step 3 of left
-    :param d1: generated in step1
-    :param k1: generated in step2
-    :param r, s2, s3: received
-    :return: signature(r, s) or 'error'
-    """
-    s = ((d1 * k1) * s2 + d1 * s3 - r) % pre_SM2.N
-    if s != 0 or s != pre_SM2.N - r: return (r, s)
-    else: return 'error'
+    # 2:计算Q、e并发送给服务器
+    myID='client'
+    hisID='server'
+    Z=myID+hisID
+    M='202000460012'
+    M_=Z+M
+    M_b = bytes(M_, encoding='utf-8')
+    e = sm3.sm3_hash(func.bytes_to_list(M_b))
+    k1 = secrets.randbelow(N)
+    Q1 = elliptic_mult(k1, G)
+    sdata = str(Q1[0]) + ',' + str(Q1[1]) + '&' + e
+    client.sendto(sdata.encode(), ("127.0.0.1", 12345))
 
-s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 3:接收r，s2，s3并计算(r,s)
+    data, addr = client.recvfrom(1024)
+    data=data.decode()
+    index1 = data.index(',')
+    index2 = data.index('&')
+    r = int(data[:index1])
+    s2 = int(data[index1 + 1:index2])
+    s3 = int(data[index2 + 1:])
+    s = ((d1 * k1) * s2 + d1 * s3 - r) % N
+    if s != 0 or s != N - r:
+        signature=(r,s)
+        print("签名为:",signature)
+    else:
+        print('error')
+        client.close()
+    client.close()
 
-
-
-d1, P1 = step1()
-data = str(P1[0]) + ',' + str(P1[1])
-s.sendto(data.encode(), ("127.0.0.1" ,12300))
-
-
-
-time.sleep(1)
-ID1 = 'ID2143'
-ID2 = 'ID12300'
-Z = ID1 + ID2
-M = 'sm2 2p sign'
-k1, Q1, e = step2(Z, M)
-data = str(Q1[0]) + ',' + str(Q1[1]) + ';' + e
-s.sendto(data.encode(), ("127.0.0.1" ,12300))
-
-
-
-data, addr=s.recvfrom(1024)
-data = data.decode()
-index1 = data.index(',')
-index2 = data.index(';')
-r = int(data[:index1])
-s2 = int(data[index1 + 1:index2])
-s3 = int(data[index2+1:])
-signa = step3(d1, k1, r, s2, s3)
-print(signa)
-
-s.close()
-print("connect closed")
+if __name__ == "__main__":
+    beginAC()
