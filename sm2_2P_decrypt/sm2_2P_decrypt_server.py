49165dcc38cd3c513081a89cdce96f337644610f
ShangWuYangLiu <105548921+ShangWuYangLiu@users.noreply.github.com>
2022-07-29 21:12:39 +0800

    Update sm2_2P_decrypt_server.py

--- sm2_2P_decrypt/sm2_2P_decrypt_server.py
+++ sm2_2P_decrypt/sm2_2P_decrypt_server.py
@@ -1,41 +1,43 @@
 import socket
 import secrets
-import pre_SM2
+from new_sm2 import *
 
-def step1(P1):
-    """step 1 of right
-    :param P1: received P1
-    :return d2:random sub private key
-    :return public_key_P: public key should be published
-    """
-    d2 = secrets.randbelow(pre_SM2.N)
-    tmp = pre_SM2.inv(d2, pre_SM2.N)
-    tmp = pre_SM2.EC_multi(tmp, P1)
-    public_key_P = pre_SM2.EC_sub(tmp, pre_SM2.G)
-    return d2, public_key_P
+def beginAC():
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind(('127.0.0.1', 12345))
+    while 1:
+        # 1:接收P1=(d1^-1)G
+        data, addr = server.recvfrom(1024)
+        message = "IP " +addr[0] + ":" + str(addr[1]) + " Connected..."
+        print(message)
+        data = data.decode()
+        index1 = data.index(',')
+        P1 = (int(data[:index1]), int(data[index1 + 1:]))
+
+        # 2:生成私钥d，公钥P
+        d2 = secrets.randbelow(N)
+        P = elliptic_sub(elliptic_mult(mod_inverse(d2, N), P1), G)
+
+        # 3:加密msg
+        msg="202000460012"
+        print("Server端加密的明文为:",msg)
+        cipher=sm2_enc(msg,P)
+
+        # 4:发送密文
+        sdata=str(cipher)
+        server.sendto(sdata.encode(), addr)
+
+        # 5:接收T1，计算T2并发送
+        data, addr = server.recvfrom(1024)
+        data = data.decode()
+        index1 = data.index(',')
+        T1 = (int(data[:index1]), int(data[index1 + 1:]))
+        T2 = elliptic_mult(mod_inverse(d2, N), T1)
+        sdata = str(T2[0]) + ',' + str(T2[1])
+        server.sendto(sdata.encode(), addr)
+        print("finished.")
+    server.close()
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', 12300))
-
-
-data, addr=s.recvfrom(1024)
-data = data.decode()
-index1 = data.index(',')
-P1 = (int(data[:index1]), int(data[index1 + 1:]))
-d2, public_key_P = step1(P1)
-data = str(public_key_P[0]) + ',' + str(public_key_P[1])
-s.sendto(data.encode(), addr)
-
-data, addr=s.recvfrom(1024)
-data = data.decode()
-index1 = data.index(',')
-T1 = (int(data[:index1]), int(data[index1 + 1:]))
-tmp = pre_SM2.inv(d2, pre_SM2.N)
-T2 = pre_SM2.EC_multi(tmp, T1)
-data = str(T2[0]) + ',' + str(T2[1])
-s.sendto(data.encode(), addr)
-
-
-s.close()
-print("---------connect closed---------")
+if __name__=="__main__":
+    beginAC()
