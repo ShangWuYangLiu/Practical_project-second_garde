befd20cdd16028fd4be293ab5a09e14858503b16
ShangWuYangLiu <105548921+ShangWuYangLiu@users.noreply.github.com>
2022-07-29 16:02:14 +0800

    Update ECDSA_pitfalls.py

--- pitfalls/EDCSA/ECDSA_pitfalls.py
+++ pitfalls/EDCSA/ECDSA_pitfalls.py
@@ -14,34 +14,34 @@ from gmssl import sm3, func
 
 # ECDSA签名,并返回签名以及k
 def ECDSA_sign_and_return_k(m, sk):
-    """ECDSA signature algorithm
+    '''
     :param m: message
     :param sk: private key
     :return signature: (r, s),k
-    """
+    '''
     while 1:
         k = secrets.randbelow(N)  # N is prime, then k <- Zn*
-        R = EC_multi(k, G)
+        R = elliptic_mult(k, G)
         r = R[0] % N  # Rx mod n
         if r != 0: break
     e = sm3.sm3_hash(func.bytes_to_list(bytes(m, encoding='utf-8')))  # e = hash(m)
     e = int(e, 16)
-    s = (inv(k, N) * (e + sk * r) % N) % N
+    s = (mod_inverse(k, N) * (e + sk * r) % N) % N
     return (r, s), k
 
 
 # 使用给定的k进行签名
 def ECDSA_sign_and_assign_k(m, k, sk):
-    """ECDSA signature algorithm
+    '''
     :param m: message
     :param sk: private key
     :return signature: (r, s),k
-    """
-    R = EC_multi(k, G)
+    '''
+    R = elliptic_mult(k, G)
     r = R[0] % N  # Rx mod n
     e = sm3.sm3_hash(func.bytes_to_list(bytes(m, encoding='utf-8')))  # e = hash(m)
     e = int(e, 16)
-    tmp1 = inv(k, N)
+    tmp1 = mod_inverse(k, N)
     tmp2 = (e + sk * r) % N
     s = tmp1 * tmp2 % N
     return (r, s), k
@@ -52,8 +52,8 @@ def ECDSA_sign_and_assign_k(m, k, sk):
 # k的泄露会导致泄露d
 def ECDSA_leaking_k():
     # Alice生成公私钥对并对消息进行签名
-    sk, pk = key_gen()  # A publish pk for others to verify
-    print("Alice的私钥为:", "0x" + hex(sk)[2:].rjust(64, '0'))
+    sk, pk = generate_key()
+    print("Alice的私钥为:",hex(sk)[2:].rjust(64, '0'))
     message_A = "message of A"
     # return k为leaking k
     signature, k = ECDSA_sign_and_return_k(message_A, sk)
@@ -62,7 +62,7 @@ def ECDSA_leaking_k():
     r, s = signature
     e = sm3.sm3_hash(func.bytes_to_list(bytes(message_A, encoding='utf-8')))  # e = hash(m)
     e = int(e, 16)
-    d = (s * k - e) % N * inv(r, N) % N
+    d = (s * k - e) % N * mod_inverse(r, N) % N
     print("Bob推测的d为:", '0x' + hex(d)[2:].rjust(64, '0'))
     # 验证Bob得到的d是否与Alice的sk相等
     print("验证Bob推测的d是否与Alice的sk相等:", True if d == sk else False)
@@ -83,7 +83,7 @@ def ECDSA_leaking_k():
 # 对不同的消息使用相同的k进行签名会泄露d
 def ECDSA_reusing_k():
     # Alice使用相同的k生成两个消息的签名
-    sk, pk = key_gen()  # A publish pk for others to verify
+    sk, pk = generate_key()  # A publish pk for others to verify
     print("Alice的私钥为:", "0x" + hex(sk)[2:].rjust(64, '0'))
     message_A1 = "message1 of A"
     message_A2 = "message2 of A"
@@ -99,7 +99,7 @@ def ECDSA_reusing_k():
     e1 = int(e1, 16)
     e2 = sm3.sm3_hash(func.bytes_to_list(bytes(message_A2, encoding='utf-8')))  # e = hash(m)
     e2 = int(e2, 16)
-    d = (((e1 - e2) * s2) % N * inv((s1 - s2) % N, N) - e2) * inv(r, N) % N
+    d = (((e1 - e2) * s2) % N * mod_inverse((s1 - s2) % N, N) - e2) * mod_inverse(r, N) % N
     print("Bob推测的d为:", '0x' + hex(d)[2:].rjust(64, '0'))
     print("验证Bob推测的d是否与Alice的sk相等:", True if d == sk else False)
     print("检验两个签名是否使用相同的k值:", True if k1 == k2 else False)
@@ -120,12 +120,12 @@ def ECDSA_reusing_k():
 def same_k_of_different_users():
     # Alice和Bob使用相同的k分别生成消息签名
     # Alice
-    sk_A, pk_A = key_gen()  # A publish pk for others to verify
+    sk_A, pk_A = generate_key()  # A publish pk for others to verify
     print("Alice的私钥为:", "0x" + hex(sk_A)[2:].rjust(64, '0'))
     message_A = "message of A"
     signature_A, k = ECDSA_sign_and_return_k(message_A, sk_A)
     # Bob
-    sk_B, pk_B = key_gen()  # A publish pk for others to verify
+    sk_B, pk_B = generate_key()  # A publish pk for others to verify
     print("Bob的私钥为:", "0x" + hex(sk_B)[2:].rjust(64, '0'))
     message_B = "message of B"
     signature_B, k = ECDSA_sign_and_assign_k(message_B, k, sk_B)
@@ -135,7 +135,7 @@ def same_k_of_different_users():
     r1, s1 = signature_A
     e1 = sm3.sm3_hash(func.bytes_to_list(bytes(message_A, encoding='utf-8')))  # e = hash(m)
     e1 = int(e1, 16)
-    d_A = (s1 * k - e1) % N * inv(r1, N) % N
+    d_A = (s1 * k - e1) % N * mod_inverse(r1, N) % N
     print("Bob推测的Alice的d为:", '0x' + hex(d_A)[2:].rjust(64, '0'))
 
     # Alice通过Bob的signature、message以及k推测Bob的d即私钥
@@ -143,7 +143,7 @@ def same_k_of_different_users():
     r2, s2 = signature_B
     e2 = sm3.sm3_hash(func.bytes_to_list(bytes(message_B, encoding='utf-8')))  # e = hash(m)
     e2 = int(e2, 16)
-    d_B = (s2 * k - e2) % N * inv(r2, N) % N
+    d_B = (s2 * k - e2) % N * mod_inverse(r2, N) % N
     print("Alice推测的Bob的d为:", '0x' + hex(d_B)[2:].rjust(64, '0'))
 
     # 验证Alice和Bob的猜测是否正确
@@ -156,12 +156,12 @@ def same_k_of_different_users():
 # 即验证(r,s) and (r,-s)均为合法签名
 def verify_Malleability():
     # Alice生成消息签名
-    sk, pk = key_gen()  # A publish pk for others to verify
+    sk, pk = generate_key()  # A publish pk for others to verify
     message = "message of A"
     signature = ECDSA_sign(message, sk)
     r, s = signature
     signature_test = (r, -s)
-    print("验证(r,-s)是否是合法签名:", True if ECDSA_verify(signature_test, message, pk) == 1 else Flase)
+    print("验证(r,-s)是否是合法签名:", True if ECDSA_verify(signature_test, message, pk) == 1 else False)
 
 
 if __name__ == '__main__':
